2dfa95ec70a3ab767ed419ecc9110437aa0e2a21
David García Alonso <g.a.david@gmail.com>
2017-10-11 13:55:37 +0200

    Añadida vista para ver un post al pulsar en el listado

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -6,5 +6,9 @@ from django.utils import timezone
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts' : posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)  # Esto es como Post.objects.get(pk=pk) pro teniendo en cuenta el 404
+    return render(request, 'blog/post_detail.html', {'post': post})
