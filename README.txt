b7e9ab929b85b9197b08804ca67fa914ed9c032b
David García Alonso <g.a.david@gmail.com>
2017-10-23 00:01:29 +0200

    Cambios adicionales para Heroku

---
+++ README.txt
@@ -0,0 +1,6 @@
+Para publicar en heroku
+
+pip install dj-database-url gunicorn whitenoise
+
+Añadir además a mano
+ psycopg2 en requirements.txt
--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jtu4(s0m_p@=w^sccaapm8w3lcct@0$-!250ky29-he#2v+0vm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['zdavidlnx.pythonanywhere.com', '127.0.0.1',]
+ALLOWED_HOSTS = ['zdavidlnx.pythonanywhere.com', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'djangogirls.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -123,3 +128,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
